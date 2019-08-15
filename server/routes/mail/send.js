905d8e5168bbf6423b1e0daa2bb2578088a00e24
Dean Zaslow <zaslowd@upmc.edu>
2019-08-15 13:31:28 -0400

    Adds rough email template

--- server/routes/mail/send.js
+++ server/routes/mail/send.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async function(req, res) {
+  const prefix = 'YMBC';
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
@@ -11,10 +12,19 @@ module.exports = async function(req, res) {
   });
 
   try {
+    // const from = `${req.body.name}, <${req.body.email}>`;
     let info = await transporter.sendMail({
+      from: `${req.body.name}, <${req.body.email}>`,
       to: req.body.to,
-      subject: req.body.subject,
-      text: req.body.text
+      subject: `${prefix}: ${req.body.subject}`,
+      html: '<h3 style=\'display:inline-block\'>From: </h3>' +
+        `<span style='display:inline-block;margin-left:.5rem'>${req.body.email}</span><br />` +
+        '<h3 style=\'display:inline-block\'>Name: </h3>' +
+        `<span style='display:inline-block;margin-left:.5rem'>${req.body.name}</span><br />` +
+        '<h3 style=\'display:inline-block\'>Subject: </h3>' +
+        `<span style='display:inline-block;margin-left:.5rem'>${req.body.subject}</span><br />` +
+        '<h3>Message: </h3>' +
+        `<p>${req.body.content}</p>`
     });
 
     console.log('Message sent: %s', info.messageId);
